31b115beef5ad981e0f2f64250e31c562f59005a
KonradKamilChwalowski <ace55@wp.pl>
2025-07-18 19:21:23 +0200

    Add some qualities

--- Entities/Components/EssenceComponent/essence_component.gd
+++ Entities/Components/EssenceComponent/essence_component.gd
@@ -1,13 +1,6 @@
 class_name EssenceComponent extends Node2D
 
-# IF MORE THAN ONE ESSENCE
-#enum Essences { FIRE, WATER, WIND, EARTH }
-#@export var activ_essence: Array[Essences] = [Essences.FIRE]
+@export var essence: game_manager.Essences = game_manager.Essences.FIRE
 
-@export var activ_essence: game_manager.Essences = game_manager.Essences.FIRE
-
-func change_essence(new_essence: game_manager.Essences) -> void:
-	activ_essence = new_essence
-
-func return_activ_essence() -> game_manager.Essences:
-	return activ_essence
+func return_essence() -> game_manager.Essences:
+	return essence
