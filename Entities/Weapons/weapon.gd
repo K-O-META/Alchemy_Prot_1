d18cc7caaaff6cb2905971786f4ad3ef50b73fba
KonradKamilChwalowski <ace55@wp.pl>
2025-07-19 22:24:02 +0200

    Fix firefighter "null" node

--- Entities/Weapons/weapon.gd
+++ Entities/Weapons/weapon.gd
@@ -2,8 +2,8 @@ class_name Weapon extends Node2D
 
 
 func animate() -> void:
-    push_error("You must override `animate()` in a subclass of Weapon.")
+	push_error("You must override `animate()` in a subclass of Weapon.")
 
 
 func rotate_toward(_target: Node2D) -> void:
-    push_error("You must override `rotate_toward()` in a subclass of Weapon.")
+	push_error("You must override `rotate_toward()` in a subclass of Weapon.")
